7f5d18bc676f0541526e75954b5d47b30350d44a
Caroalv <alvaradocarolinad2002@gmail.com>
2024-09-18 18:59:50 -0600

    Router

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="flex flex-col min-h-screen">
+    <!-- Header -->
+    <header class="flex items-center h-16 px-6 bg-white border-b border-gray-300 shadow">
+      <div>
+        <a class="flex items-center gap-2 font-semibold" href="#">
+          <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="40" height="40" />
+          <span class="text-lg font-bold">Mi Sitio Web</span>
+        </a>
+      </div>
+      <nav class="ml-auto space-x-6 flex items-center h-full">
+        <RouterLink to="/" class="hover:text-blue-500">Principal</RouterLink>
+        <RouterLink :to="{ name: 'FeaturesPagina' }" class="hover:text-blue-500">Feature</RouterLink>
+        <RouterLink :to="{ name: 'ContactoPagina' }" class="hover:text-blue-500">Contacto</RouterLink>
+        <RouterLink :to="{ name: 'PreciosPagina' }" class="hover:text-blue-500">Precio</RouterLink>
+      </nav>
+    </header>
+    <!-- Fin Header -->
+
+    <!-- Main -->
+    <main class="flex-grow">
+      <RouterView />
+    </main>
+    <!-- Fin Main -->
+
+    <!-- Footer -->
+    <footer class="flex items-center h-16 px-6 bg-gray-100 border-t border-gray-300">
+      <p class="text-sm text-gray-500 text-center w-full">
+        © 20xx Acme Corporation. Derechos reservados.
+      </p>
+    </footer>
+    <!-- Fin Footer -->
+  </div>
+</template>
+
+<script lang="ts" setup>
+</script>
---
+++ src/modules/landing/pages/PrincipalPagina.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="min-h-screen flex items-center justify-center bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500">
+    <div class="text-center">
+      <h1 class="text-4xl md:text-6xl font-bold text-white animate-fade-in">
+        ¡Bienvenido!
+      </h1>
+      <p class="mt-4 text-lg md:text-2xl text-gray-200">
+        Es un placer tenerte aquí, ¡exploremos juntos!
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Principal",
+};
+</script>
+
+<style scoped>
+@keyframes fade-in {
+  0% {
+    opacity: 0;
+    transform: translateY(-20px);
+  }
+  100% {
+    opacity: 1;
+    transform: translateY(0);
+  }
+}
+
+.animate-fade-in {
+  animation: fade-in 2s ease-in-out;
+}
+</style>
